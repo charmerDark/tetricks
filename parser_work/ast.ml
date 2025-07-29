c524bd52341553f15841e812d12888c637f28a04
vishnu <vishnuajith@gmail.com>
2025-07-29 23:51:37 +0100

    changed parser structure to work as a two stage pipeline with cpp middlend

--- parser_work/ast.ml
+++ parser_work/ast.ml
@@ -3,13 +3,16 @@ type bop =
 | Subtract
 | Multiply
 | Divide
+[@@deriving yojson]
 
 type expr =
 | Einsum of einsum
 | BinOp of bop * expr * expr
 | Tensor of string
+[@@deriving yojson]
 
 and einsum = {
   notation: string;
   tensors: expr list;
 }
+[@@deriving yojson]
